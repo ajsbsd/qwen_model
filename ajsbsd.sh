50c5e46a03c98fa0f6d04f6cd2df79cbc1b3aa93
ajsbsd@gmail.com <ajsbsd@gmail.com>
2025-05-11 01:03:44 +0000

    0

---
+++ ajsbsd.sh
@@ -0,0 +1,2 @@
+git config --global user.email "ajsbsd@gmail.com"
+git config --global user.name "ajsbsd@gmail.com"
--- userAi.sh
+++ userAi.sh
@@ -3,6 +3,9 @@ useradd ai
 mkdir /home/ai
 chown ai:ai ~ai
 usermod -s /bin/bash ai
-cp -Rpp /notebooks/2025-05-10/* ~ai
+cp -Rpp /notebooks/2025-05-10.1/* ~ai
 cp -Rpp /notebooks/user/dot.profile /home/ai/.profile
 chown ai:ai /home/ai/.profile
+sudo -u ai -i ./0.sh
+#sudo -u ai -i "uvicorn main:app --host 0.0.0.0 --port 5000"
+#sudo -u ai bash -c "cd /home/ai && nohup uvicorn main:app --host 0.0.0.0 --port 5000"
