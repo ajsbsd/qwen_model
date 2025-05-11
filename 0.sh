806cabc9b3ecb7045cf8c156430c685839340fbe
ajsbsd@gmail.com <ajsbsd@gmail.com>
2025-05-11 00:18:01 +0000

    0.sh

--- 0.sh
+++ 0.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 python -m venv .
 source /home/ai/bin/activate
+#!/bin/bash
+pip install --upgrade pip
+pip install --upgrade torch transformers accelerate sentencepiece
+cp dot.profile ~/.profile
+pip install --upgrade flask
+ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
+#!/bin/bash
+mkdir -p ~/qwen_model
+cd ~/qwen_model
+git lfs install
+git clone https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct  .
+git config --global credential.helper store
--- 1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install --upgrade pip
-pip install --upgrade torch transformers accelerate sentencepiece
-cp dot.profile ~/.profile
-pip install --upgrade flask
-ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
